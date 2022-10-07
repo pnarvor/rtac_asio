c3c5e3c5fb3f39a63a33c60bd010bc1bb8a7a951
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2022-10-07 11:50:06 +0200

    Added generic Stream class with read/write methods

--- tests/src/serialstream02.cpp
+++ tests/src/serialstream02.cpp
@@ -8,6 +8,8 @@ using namespace std::placeholders;
 #include <rtac_asio/Stream.h>
 using namespace rtac::asio;
 
+std::string msg = "Hello there !\n";
+
 void write_callback(const SerialStream::ErrorCode& err,
                     std::size_t writeCount)
 {
@@ -21,8 +23,10 @@ void read_callback(Stream* serial,
 {
     std::cout << "Got data (" << count
               << " bytes) : " << *data << std::endl;
-    serial->async_read_some(data->size(), (uint8_t*)data->c_str(),
-                            std::bind(&read_callback, serial, data, _1, _2));
+    //serial->async_read_some(data->size(), (uint8_t*)data->c_str(),
+    //                        std::bind(&read_callback, serial, data, _1, _2));
+    serial->async_read(msg.size() + 1, (uint8_t*)data->c_str(),
+                       std::bind(&read_callback, serial, data, _1, _2));
 }
 
 int main()
@@ -32,17 +36,21 @@ int main()
     auto service = AsyncService::Create();
 
     Stream serial(SerialStream::Create(service, "/dev/ttyACM0"));
-    serial.async_read_some(data.size(), (uint8_t*)data.c_str(),
-                           std::bind(&read_callback, &serial, &data, _1, _2));
+    //serial.async_read_some(data.size(), (uint8_t*)data.c_str(),
+    //                       std::bind(&read_callback, &serial, &data, _1, _2));
+    serial.async_read(msg.size() + 1, (uint8_t*)data.c_str(),
+                      std::bind(&read_callback, &serial, &data, _1, _2));
 
     service->start();
     
-    std::string msg = "Hello there !\n";
     while(1) {
         getchar();
-        serial.async_write_some(msg.size() + 1,
-                                (const uint8_t*)msg.c_str(),
-                                &write_callback);
+        //serial.async_write_some(msg.size() + 1,
+        //                        (const uint8_t*)msg.c_str(),
+        //                        &write_callback);
+        serial.async_write(msg.size() + 1,
+                           (const uint8_t*)msg.c_str(),
+                           &write_callback);
     }
 
     service->stop();
