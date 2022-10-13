1bd8116b1e58c0bd1337e26f5016b4711cfef8dc
Pierre Narvor <pierre.narvor@ensta-bretagne.fr>
2022-10-13 15:56:54 +0200

    Added StreamReader type

--- tests/src/serial_stream_reader01.cpp
+++ tests/src/serial_stream_reader01.cpp
@@ -5,7 +5,7 @@ using namespace std::placeholders;
 
 #include <rtac_asio/AsyncService.h>
 #include <rtac_asio/SerialStream.h>
-#include <rtac_asio/Stream.h>
+#include <rtac_asio/StreamReader.h>
 using namespace rtac::asio;
 
 std::string msg = "Hello there !\n";
@@ -16,16 +16,15 @@ void write_callback(const SerialStream::ErrorCode& err,
     std::cout << "Wrote data (" << writeCount << " bytes)." << std::endl;
 }
 
-void read_callback(Stream::Ptr serial,
+void read_callback(StreamReader::Ptr stream,
                    std::string* data,
                    const SerialStream::ErrorCode& err,
                    std::size_t count)
 {
     std::cout << "Got data (" << count
               << " bytes) : " << *data << std::endl;
-    serial->async_read(msg.size(), (uint8_t*)data->c_str(),
-                       std::bind(&read_callback, serial, data, _1, _2),
-                       1000);
+    stream->async_read_some(data->size(), (uint8_t*)data->c_str(),
+                            std::bind(&read_callback, stream, data, _1, _2));
 }
 
 int main()
@@ -33,27 +32,22 @@ int main()
     std::string data(1024, '\0');
 
     auto service = AsyncService::Create();
+    auto serial = SerialStream::Create(service, "/dev/ttyACM0");
+    auto stream = StreamReader::Create(serial);
 
-    auto serial = Stream::Create(SerialStream::Create(service, "/dev/ttyACM0"));
-    serial->async_read(msg.size(), (uint8_t*)data.c_str(),
-                      std::bind(&read_callback, serial, &data, _1, _2));
+    serial->async_read_some(data.size(), (uint8_t*)data.c_str(),
+                            std::bind(&read_callback, stream, &data, _1, _2));
 
     service->start();
     std::cout << "Started" << std::endl;
     
     while(1) {
-    //for(int i = 0; i < 5; i++) {
         getchar();
-        serial->async_write(msg.size(),
-                           (const uint8_t*)msg.c_str(),
-                           &write_callback);
+        serial->async_write_some(msg.size(),
+                                 (const uint8_t*)msg.c_str(),
+                                 &write_callback);
         std::cout << "Service running ? : " << !service->stopped() << std::endl;
     }
-    serial->async_read(msg.size(), (uint8_t*)data.c_str(),
-                      std::bind(&read_callback, serial, &data, _1, _2));
-    
-    cout << "There " << endl;
-    getchar();
 
     service->stop();
 
